5c7b7c91137e7856e6f0f9d3b43bd39417ccd693
Felix Maury <femaury@student.42.fr>
2021-06-16 18:31:45 +0200

    :package: Updated package version in download link

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Deepsense",
-            url: "https://github.com/thedeepsense/sdk-ios/releases/download/0.3.3/Deepsense-v0.3.3.zip",
-            checksum: ""
+            url: "https://github.com/thedeepsense/sdk-ios/releases/download/0.3.4/Deepsense-v0.3.4.zip",
+            checksum: "3143303c201394889ba5f756d5dce24b825989d414c1c512f341072e629b0891"
         )
     ]
 )
