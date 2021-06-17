7b13fc6645231d967e9f88df2b83208c4a319362
Felix Maury <femaury@student.42.fr>
2021-06-17 11:42:15 +0200

    :package: Fixed binary name

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "Deepsense",
+            name: "Deepsense-v0.3.4",
             url: "https://www.dropbox.com/s/ty19r57lno97k36/Deepsense-v0.3.4.xcframework.zip?dl=1",
             checksum: "3143303c201394889ba5f756d5dce24b825989d414c1c512f341072e629b0891"
         )
