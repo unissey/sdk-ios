646f721f040512e71257116861848e34ec2a6e95
Felix Maury <femaury@student.42.fr>
2021-06-24 16:43:04 +0200

    :bookmark: Bump version to 0.4.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Deepsense",
-            url: "https://www.dropbox.com/s/areqh0ctuqp7q0t/Deepsense-v0.4.0.xcframework.zip?dl=1",
-            checksum: "1ef3e4bbbfcd2b6a4a39aec8f5e167106a4dff00edbcdeebde90e1ec92d9bc2e"
+            url: "https://www.dropbox.com/s/5ob1jf1x98ho357/Deepsense-v0.4.1.xcframework.zip?dl=1",
+            checksum: "db6d373e8ad8440b318e6d7ba595e09da56e10d50459f3558963e968df5adf5d"
         )
     ]
 )
