6f3ccfd4f0e402bc592812bce31728dbd34f84ef
Felix Maury <femaury@student.42.fr>
2021-06-24 16:54:03 +0200

    :construction: Working on sample app

--- sample-app/sample-app/ContentView.swift
+++ sample-app/sample-app/ContentView.swift
@@ -5,20 +5,33 @@
 //  Created by Felix Maury on 24/06/2021.
 //
 
-import SwiftUI
 import Deepsense
+import SwiftUI
+import AVKit
 
 struct ContentView: View {
-    @State var video: Data?
-    
+    @State var videoUrl: URL?
+
     var body: some View {
         NavigationView {
             VStack {
-                DSWidgetView(onVideoCapture: { capture in
-//                    self.video = capture
-                })
+                if let videoUrl = videoUrl {
+                    Spacer()
+                    VideoPlayer(player: AVPlayer(url: videoUrl))
+                        .frame(height: 400)
+                    Spacer()
+                } else {
+                    DSWidgetView(onVideoCapture: { capture in
+                        guard var url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+                        url.appendPathComponent("video.mpeg") // or whatever extension the video is
+                        if ((try? capture.video.write(to: url)) != nil) {
+                            self.videoUrl = url
+                        }
+                    })
+                }
             }
         }
+        .navigationBarHidden(true)
     }
 }
 
