856d5df2c61c3abedcb8b2533c414c177d18d5c5
Guillaume Depardon <guillaume.depardon@gmail.com>
2021-07-29 17:52:05 +0200

    Bump to version 1.0.2

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Deepsense",
-            url: "https://www.dropbox.com/s/5ob1jf1x98ho357/Deepsense-v0.4.1.xcframework.zip?dl=1",
-            checksum: "db6d373e8ad8440b318e6d7ba595e09da56e10d50459f3558963e968df5adf5d"
+            url: "https://storage.googleapis.com/deepsense-shared/sdk-ios/Deepsense-v1.0.2.xcframework.zip",
+            checksum: "34d9d4d059a646fa2ed7b882b360b7123858aa1e7135b705d725a949554eeec4"
         )
     ]
 )
