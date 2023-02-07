b9bdcb5e322bf0a3d760ac866069c2b6ed4ed99a
Olivier <oroblin@gmail.com>
2023-02-07 19:11:03 +0100

    🔖 use remote distribution

--- Package.swift
+++ Package.swift
@@ -15,16 +15,16 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-//  This doesn't work : we get an error : 'downloaded archive of binary target does not contain expected binary artifact'
+        .binaryTarget(
+            name: "UnisseySDK",
+            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySDK-v2.0.0.xcframework.zip",
+            checksum: "bae72d808b8dadc418aabcdd554ae42c65ac63fe72d3b42f969c5b4e29e7fc76"
+        ),
+        
+        // [Local distribution]
 //        .binaryTarget(
 //            name: "UnisseySDK",
-//            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySDK-v2.0.0.xcframework.zip",
-//            checksum: "5fbe9bb82c915e85c47756c292d9e4c7ca7b2c4e6d6f531d6dbe13a8b951a218"
+//            path: "./dist/UnisseySDKLocal.xcframework"
 //        ),
-        
-            .binaryTarget(
-                name: "UnisseySDK",
-                path: "./dist/UnisseySDK.xcframework"
-            ),
     ]
 )
