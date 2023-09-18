95c3f5db3c269af944125210b7b1c3d33cce76da
OlivierR1 <oroblin@gmail.com>
2023-09-18 19:11:46 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,28 +3,23 @@ import PackageDescription
 
 
 let package = Package(
-    name: "UnisseySDK",
+    name: "UnisseySdk",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14)
     ],
     products: [
         .library(
-            name: "UnisseySDK",
-            targets: ["UnisseySDK"])
+            name: "UnisseySdk",
+            targets: ["UnisseySdk"])
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(
             name: "UnisseySDK",
-            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySDK-v2.0.0.xcframework.zip",
-            checksum: "bae72d808b8dadc418aabcdd554ae42c65ac63fe72d3b42f969c5b4e29e7fc76"
+            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySDK-v4.0.0.xcframework.zip",
+            checksum: "e631f049efdf253a7ec577cc3b466e0331c4aae251f3c30fe17c82b9b6fb01aa"
         ),
-        
-        // [Local distribution]
-//        .binaryTarget(
-//            name: "UnisseySDK",
-//            path: "./dist/UnisseySDKLocal.xcframework"
-//        ),
     ]
 )
+
