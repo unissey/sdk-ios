97096ccb2a54052e9fa08f424cde52e27a203736
Paul-Etienne Francois <paul-etienne.francois@unissey.com>
2023-09-19 10:55:59 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UnisseySdk",
-            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySDK-v4.0.0.xcframework.zip",
-            checksum: "e631f049efdf253a7ec577cc3b466e0331c4aae251f3c30fe17c82b9b6fb01aa"
+            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySdk-v4.0.0.xcframework.zip",
+            checksum: "c8c3b2d1dcacd1f7c8d55e9efe2e5e33cd5a7f1788f23496293126ce81f0ccfd"
         ),
         
         // [Local distribution]
