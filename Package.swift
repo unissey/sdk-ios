8e756d07036fad45b66e0f94d50cfced99b439a2
Paul-Etienne Francois <paul-etienne.francois@unissey.com>
2023-09-19 10:25:47 +0200

    Revert "Merge pull request #1 from unissey/v4.0.0"
    This reverts commit 2bcf8ead4c821ccd586c09582545179b60537268, reversing
    changes made to 05e3f85c6f4e8bc666c31da27395affacc2d99d9.

--- Package.swift
+++ Package.swift
@@ -3,23 +3,28 @@ import PackageDescription
 
 
 let package = Package(
-    name: "UnisseySdk",
+    name: "UnisseySDK",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v13)
     ],
     products: [
         .library(
-            name: "UnisseySdk",
-            targets: ["UnisseySdk"])
+            name: "UnisseySDK",
+            targets: ["UnisseySDK"])
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(
             name: "UnisseySDK",
-            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySDK-v4.0.0.xcframework.zip",
-            checksum: "e631f049efdf253a7ec577cc3b466e0331c4aae251f3c30fe17c82b9b6fb01aa"
+            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySDK-v2.0.0.xcframework.zip",
+            checksum: "bae72d808b8dadc418aabcdd554ae42c65ac63fe72d3b42f969c5b4e29e7fc76"
         ),
+        
+        // [Local distribution]
+//        .binaryTarget(
+//            name: "UnisseySDK",
+//            path: "./dist/UnisseySDKLocal.xcframework"
+//        ),
     ]
 )
-
