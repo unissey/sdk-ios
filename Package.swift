d3538c8d23ba751651b85524e31db9c5c985dc37
Paul-Etienne Francois <paul-etienne.francois@unissey.com>
2023-09-22 11:40:05 +0200

    chore: Update the zip checksum

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "UnisseySdk",
             url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySdk-v4.0.0.xcframework.zip",
-            checksum: "c8c3b2d1dcacd1f7c8d55e9efe2e5e33cd5a7f1788f23496293126ce81f0ccfd"
+            checksum: "08db4805da529a83181f562cc4b12dc1ee93760b6f20f33dd18e9ac7d3a19495"
         ),
         
         // [Local distribution]
