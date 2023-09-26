3577b94b781fb2a1d30171707d63b38b43b0e716
Paul-Etienne Francois <paul-etienne.francois@unissey.com>
2023-09-26 14:40:25 +0200

    chore: Update Package.swift after rebase

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 
 let package = Package(
-    name: "UnisseySDK",
+    name: "UnisseySdk",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14)
     ],
     products: [
         .library(
-            name: "UnisseySDK",
-            targets: ["UnisseySDK"])
+            name: "UnisseySdk",
+            targets: ["UnisseySdk"])
     ],
     dependencies: [
     ],
@@ -20,11 +20,5 @@ let package = Package(
             url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySdk-v4.0.0.xcframework.zip",
             checksum: "fcc11396c90144847c55ba4aaad583ecfefcf6d48525f076b902aeef96aaffa3"
         ),
-        
-        // [Local distribution]
-//        .binaryTarget(
-//            name: "UnisseySDK",
-//            path: "./dist/UnisseySDKLocal.xcframework"
-//        ),
     ]
 )
