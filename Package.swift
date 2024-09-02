b5df1a56f76fcccca29f3abc1ddf3e4e4e305bf9
Paul-Etienne Francois <paul-etienne.francois@unissey.com>
2024-09-02 15:10:45 +0200

    feat: Add version 3.1.4 of the iOS SDK, allowing buttons' corner radius customization

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UnisseySdk",
-            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySdk-v3.1.3.xcframework.zip",
-            checksum: "e7a9af1e6c119ac60a082521febe060545d98f8355ab12e0be460ced353f8407"
+            url: "https://storage.googleapis.com/unissey-shared/sdk-ios/UnisseySdk-v3.1.4.xcframework.zip",
+            checksum: "75f3e1a078d9cddfb5b0ea81be274379017d0426d2927fd2f4785b002e4b8243"
         ),
     ]
 )
